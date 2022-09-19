cd9ab545cde82cc095f1d476508ea12291f042a9
guneysrhat <guneysrhat@gmail.com>
2022-09-19 20:53:43 +0300

    created uniq id for li

--- app.js
+++ app.js
@@ -1,3 +1,32 @@
 //* ======================================================
 //*                     TODO APP
 //* ======================================================
+
+// const saat = new Date().getTime();
+// console.log(saat);
+
+//? Selectors
+
+const addBtn = document.getElementById("todo-button");
+const todoInput = document.getElementById("todo-input");
+const todoUl = document.getElementById("todo-ul");
+
+addBtn.addEventListener("click", () => {
+  if (todoInput.value.trim() === "") {
+    alert("Please enter new todo");
+  } else {
+    const newTodo = {
+      id: new Date().getTime(),
+      completed: false,
+      text: todoInput.value,
+    };
+    createListElement(newTodo);
+  }
+});
+
+const createListElement = (newTodo) => {
+  const li = document.createElement("li");
+  //   li.id = newTodo.id;
+  li.setAttribute("id", newTodo.id);
+  console.log(li);
+};
